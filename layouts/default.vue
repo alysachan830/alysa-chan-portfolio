56eb4a9812f17942000e3f1b76e66c65b1ac132e
alysachan <alysachansl@gmail.com>
2021-10-21 15:20:19 +0800

    feat: add bootstrap and banner

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav class="d-flex justify-content-between my-10 container">
+      <h1 class="font-l">
+        <NuxtLink to="/">Alysa Chan</NuxtLink>
+      </h1>
+      <ul class="d-flex">
+        <li class="me-16">
+          <a class="fw-medium text-uppercase" href="#">works</a>
+        </li>
+        <li class="me-16">
+          <a class="fw-medium text-uppercase" href="#">articles</a>
+        </li>
+        <li>
+          <a class="fw-medium text-uppercase" href="#">about</a>
+        </li>
+      </ul>
+    </nav>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+nav {
+  z-index: 10;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,116 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div class="position-relative vh-100 vw-100 mb-20">
+      <div
+        class="
+          banner-bg
+          position-absolute
+          vw-100
+          vh-100
+          d-flex
+          justify-content-center
+          align-items-center
+        "
+      >
+        <div class="text-center">
+          <img class="banner-icon" src="@/assets/images/dev_icon.png" alt="" />
+          <h2 class="text-uppercase fw-bold banner-bg__title">alysa chan</h2>
+          <h3 class="text-info mb-16">Front-end developer</h3>
+          <ul class="d-flex justify-content-center mb-20">
+            <li class="me-10">
+              <a href="#">
+                <img
+                  class="social-media-icon"
+                  src="@/assets/images/5282542_linkedin_network_social network_linkedin logo_icon.svg"
+                  alt=""
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img
+                  class="social-media-icon"
+                  src="@/assets/images/317712_code repository_github_repository_resource_icon.svg"
+                  alt=""
+                />
+              </a>
+            </li>
+          </ul>
+          <a href="#" class="btn px-14 btn-primary btn-lg">All works</a>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <h2 class="fw-bold font-2xl mb-20">Works</h2>
+      <ul>
+        <li v-for="work in works" :key="work.title">
+          <a href="#" class="row justify-content-between mb-20">
+            <div class="col-md-7 mb-5 mb-md-0">
+              <div class="img--hover--zoom">
+                <img :src="`${work.img}`" alt="" />
+              </div>
+            </div>
+            <div class="col-md-4">
+              <h2 class="mb-5">{{ work.title }}</h2>
+              <ul class="d-flex text-info">
+                <li v-for="skill in work.skills" :key="skill" class="me-5">
+                  {{ skill }}
+                </li>
+              </ul>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      works: [
+        {
+          title: 'Marugo Class Web App & LINE Chatbot',
+          img: '~/assets/images/marugo-mock-up.jpg',
+          skills: ['Vue.js', 'Express.js', 'Bootstrap 5'],
+        },
+        {
+          title: 'Flash Ticketing',
+          img: '~/assets/images/marugo-mock-up.jpg',
+          skills: ['Nuxt.js', 'Express.js', 'Bootstrap 5'],
+        },
+        {
+          title: 'Deluxury Website Design',
+          img: '~/assets/images/marugo-mock-up.jpg',
+          skills: ['UI/UX design'],
+        },
+      ],
+    }
+  },
+}
 </script>
+
+<style lang="scss" scoped>
+.banner-icon {
+  height: 140px;
+  width: 140px;
+  margin: 0 auto;
+}
+
+.banner-bg {
+  background: linear-gradient(#ffffff, #c9cdff);
+  top: 0;
+  left: 0;
+
+  &__title {
+    font-size: 92px;
+    letter-spacing: 4px;
+  }
+}
+
+.social-media-icon {
+  width: 30px;
+  height: 30px;
+}
+</style>
