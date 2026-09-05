62fddb99367ab92f02682ff77f0a33f53b457f6a
Alysa Chan <alysachansl@gmail.com>
2026-09-06 07:19:36 +0800

    Remove menu icon in mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,7 +14,7 @@
         <h1>
           <NuxtLink to="/" class="font-md-l font-m">Alysa Chan</NuxtLink>
         </h1>
-        <ul class="d-none d-md-flex align-items-center">
+        <ul class="d-flex align-items-center">
           <li class="me-16">
             <NuxtLink to="/about" class="hover hover--primary fw-medium text-uppercase">about</NuxtLink>
           </li>
@@ -37,32 +37,8 @@
             </a>
           </li>
         </ul>
-        <button
-          :class="[
-            { 'top-nav-toggle--cross': showMenu },
-            'top-nav-toggle d-md-none',
-          ]"
-          @click="showMenu = !showMenu"
-        ></button>
       </nav>
     </div>
-    <transition name="slide">
-      <div v-show="showMenu" class="menu py-16 bg-primary d-md-none">
-        <div class="container">
-          <ul>
-            <li>
-              <a
-                href="/about"
-                class="text-uppercase font-md-4xl font-3xl fw-bold text-white"
-                @click="showMenu = false"
-              >
-                About
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </transition>
     <div>
       <Nuxt />
       <transition name="fade">
@@ -80,11 +56,6 @@
 
 <script>
 export default {
-  data() {
-    return {
-      showMenu: false,
-    }
-  },
   methods: {
     scrollToTop() {
       document.body.scrollTop = 0 // For Safari
@@ -106,72 +77,6 @@ export default {
   }
 }
 
-.slide-enter-active {
-  transition: all 0.4s ease;
-}
-.slide-leave-active {
-  transition: all 0.4s ease;
-}
-
-.slide-enter, .slide-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
-  transform: translateY(-100%);
-}
-
-.top-nav-toggle {
-  border-top: 3px solid #000000;
-  height: 18px;
-  width: 36px;
-  border-width: 3px 0 0 0;
-  background: transparent;
-  position: relative;
-
-  &::before,
-  &::after {
-    transition: transform 0.3s;
-    content: '';
-    position: absolute;
-    background: #000000;
-    height: 3px;
-    width: 100%;
-    left: 0;
-  }
-
-  &::before {
-    top: 6px;
-  }
-
-  &::after {
-    top: 15px;
-  }
-
-  &--white {
-    &::before,
-    &::after {
-      background: #ffffff;
-    }
-  }
-
-  &--cross {
-    border-top: 0px solid #000000;
-    &::before {
-      transform: rotate(45deg);
-    }
-    &::after {
-      top: 6px;
-      transform: rotate(135deg);
-    }
-  }
-}
-
-.menu {
-  position: fixed;
-  z-index: 20;
-  top: 64px;
-  bottom: 0;
-  left: 0;
-  right: 0;
-}
 
 .icon--white {
   transition: filter 0.3s;
